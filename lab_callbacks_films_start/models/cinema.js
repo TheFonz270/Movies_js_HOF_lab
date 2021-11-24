3b414dde4309b3c379b7297ad82ab3d33c7824e9
Martin Quinn <thefonz270@hotmail.com>
2021-11-24 15:16:36 +0000

    MVP and extensions done

--- lab_callbacks_films_start/models/cinema.js
+++ lab_callbacks_films_start/models/cinema.js
@@ -2,4 +2,41 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.findTitles = function () {
+let result = this.films.map(a => a.title);
+return result;
+}
+
+Cinema.prototype.findByTitle = function(title){
+  let result = this.films.find(element => title == element.title)
+  return result;
+}
+
+Cinema.prototype.filterByGenre = function(genre){
+  let result = this.films.filter(element => element.genre == genre)
+  return result;
+}
+
+Cinema.prototype.filmFromYear = function(year){
+  let result = this.films.some(element => element.year == year);
+  return result;
+}
+
+Cinema.prototype.filmLength = function(length){
+  let result = this.films.every(element => element.length > length);
+  return result;
+}
+
+Cinema.prototype.filmTotalLength = function(length){
+  let list = this.films.map(a => a.length);
+  let result = list.reduce((previousValue, currentValue) => previousValue + currentValue);
+  return result;
+}
+
+Cinema.prototype.filmsByProperty = function(property, value){
+  let result = this.films.filter(element => element[property] == value)
+  return result;
+}
+
+
 module.exports = Cinema;
